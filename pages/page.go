bb31bb2f335b292febd4112ae71aa42cfa9dc7ab
Regis Verpillot <rverpi90@gmail.com>
2024-04-18 10:14:32 +0200

    .

--- pages/page.go
+++ pages/page.go
@@ -14,23 +14,5 @@ var TemplatesFs embed.FS
 var ResourcesFs embed.FS
 
 func newAceTemplate(name string, model interface{}) *ihui.PageAce {
-	content, err := TemplatesFs.ReadFile(path.Join("templates", name))
-	if err != nil {
-		panic(err)
-	}
-	return ihui.NewPageAce(name, content, model)
-}
-
-type Page struct {
-	tmpl *ihui.PageAce
-}
-
-func NewPage(pageTemplate string, model interface{}) *Page {
-	return &Page{
-		tmpl: newAceTemplate(pageTemplate, model),
-	}
-}
-
-func (p *Page) Render(page *ihui.Page) {
-	p.tmpl.Render(page)
+	return ihui.NewPageAce(TemplatesFs, path.Join("templates", name), model)
 }
