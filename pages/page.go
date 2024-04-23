3c67989673dab07778937f68ebdfdb89a6b65653
Regis Verpillot <rverpi90@gmail.com>
2024-04-23 17:17:43 +0200

    .

--- pages/page.go
+++ pages/page.go
@@ -2,10 +2,24 @@ package pages
 
 import (
 	"embed"
+	"io/fs"
+	"log"
+	"os"
 )
 
 //go:embed templates
-var TemplatesFs embed.FS
+var EmbedTemplatesFs embed.FS
 
 //go:embed statics
-var ResourcesFs embed.FS
+var EmbedResourcesFs embed.FS
+
+var (
+	TemplatesFs fs.FS = EmbedTemplatesFs
+	ResourcesFs fs.FS = EmbedResourcesFs
+)
+
+func SetDebugMode() {
+	log.Print("Loading pages from filesystem")
+	TemplatesFs = os.DirFS("pages")
+	ResourcesFs = os.DirFS("pages")
+}
