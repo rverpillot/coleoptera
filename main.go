722d7cd70a69daea07113de7c693714a86b13911
Regis Verpillot <rverpi90@gmail.com>
2024-04-23 17:46:37 +0200

    .

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func start(session *ihui.Session) error {
 	menu.Add("especes", "Espèces", pages.NewPageEspeces(menu))
 	menu.Add("individus", "Individus", pages.NewPageIndividus(menu))
 	menu.Add("plan", "Plan", pages.NewPagePlan(menu))
-	if err := session.ShowPage("menu", menu, nil); err != nil {
+	if err := session.ShowPage("menu", menu, &ihui.Options{AlwaysVisible: true}); err != nil {
 		return err
 	}
 	return menu.ShowPage(session, "individus")
